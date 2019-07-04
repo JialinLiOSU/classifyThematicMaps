5e61f1b963e2ccee8d234c593924a5243eef180e
JialinLiOSU <32602256+JialinLiOSU@users.noreply.github.com>
2019-07-04 16:35:06 -0400

    automatically download images

---
+++ downloadImages/imageDownload.py
@@ -0,0 +1,42 @@
+# use google images api to download map images
+# importing google_images_download module 
+from google_images_download import google_images_download  
+def downloadimages(query): 
+    arguments = {"keywords": query, "format": "jpg", "limit":200, 
+                    "print_urls":True, "size": "medium",
+                    "chromedriver":"C:\\Program Files (x86)\\Google\\Chrome\\chromedriver\\chromedriver.exe"} 
+    try: 
+        response.download(arguments) 
+      
+    # Handling File NotFound Error     
+    except FileNotFoundError:  
+        arguments = {"keywords": query, 
+                     "format": "jpg", 
+                     "limit":200, 
+                     "print_urls":True,  
+                     "size": "medium",
+                     "chromedriver":"C:\\Program Files (x86)\\Google\\Chrome\\chromedriver\\chromedriver.exe"} 
+                       
+        # Providing arguments for the searched query 
+        try: 
+            # Downloading the photos based 
+            # on the given arguments 
+            response.download(arguments)  
+        except: 
+            pass
+  
+# creating object 
+response = google_images_download.googleimagesdownload()  
+  
+search_queries = ['circle proportional symbol map', 
+'choropleth map', 
+'isarithmic map', 
+'dot distribution map', 
+'flow map'
+] 
+  
+# Driver Code 
+for query in search_queries: 
+    downloadimages(query)  
+    print()  
+
